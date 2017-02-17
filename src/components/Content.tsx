846a1616f0eb2d283cf2c0fad6b08fd055ff4b77
Meg Drouhard <meg.drouhard@gmail.com>
2017-02-17 13:11:29 -0800

    updated segment grid

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Menu, Segment } from 'semantic-ui-react';
+import { Menu, Segment, Grid } from 'semantic-ui-react';
 import { observer } from 'mobx-react';
 
 import { IAppStoreProps } from '../stores/appStore'
@@ -21,6 +21,8 @@ export class Content extends React.Component<IContentProps, IContentState> {
     // _handleItemClick = (name: string) => this.setState({ activeItem: name })
 
 
+    // private _handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+
     private _handleItemClick(newStateName: string): void {
         this.setState({ activeItem: newStateName });
     }
@@ -28,16 +30,22 @@ export class Content extends React.Component<IContentProps, IContentState> {
     public render() {
         return (
             <div>
-                <Menu pointing vertical>
-                    <Menu.Item name='overview' active={this.state.activeItem === 'overview'} onClick={this._handleItemClick.bind(this, 'overview')} />
-                    <Menu.Item name='coding' active={this.state.activeItem === 'coding'} onClick={this._handleItemClick.bind(this, 'coding')} />
-                    <Menu.Item name='features' active={this.state.activeItem === 'features'} onClick={this._handleItemClick.bind(this, 'features')} />
-                    <Menu.Item name='disagreement' active={this.state.activeItem === 'disagreement'} onClick={this._handleItemClick.bind(this, 'disagreement')} />
-                </Menu>
+                <Grid>
+                    <Grid.Column width={2}>
+                        <Menu fluid vertical tabular>
+                            <Menu.Item name='overview' active={this.state.activeItem === 'overview'} onClick={this._handleItemClick.bind(this, 'overview')} />
+                            <Menu.Item name='coding' active={this.state.activeItem === 'coding'} onClick={this._handleItemClick.bind(this, 'coding')} />
+                            <Menu.Item name='features' active={this.state.activeItem === 'features'} onClick={this._handleItemClick.bind(this, 'features')} />
+                            <Menu.Item name='disagreement' active={this.state.activeItem === 'disagreement'} onClick={this._handleItemClick.bind(this, 'disagreement')} />
+                        </Menu>
+                    </Grid.Column>
 
-                <Segment>
-                    {React.Children.map(this.props.children, (child: React.ReactElement<any>) => React.cloneElement(child, { store: this.props.store }))}
-                </Segment>
+                    <Grid.Column stretched width={14}>
+                        <Segment>
+                            {React.Children.map(this.props.children, (child: React.ReactElement<any>) => React.cloneElement(child, { store: this.props.store }))}
+                        </Segment>
+                    </Grid.Column>
+                </Grid>
             </div>
         );
     }
