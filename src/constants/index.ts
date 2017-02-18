823c634738f3aaa0fcbac29125b6fedaa070a4ed
Meg Drouhard <meg.drouhard@gmail.com>
2017-02-17 17:40:28 -0800

    remove unused code

--- src/constants/index.ts
+++
@@ -1,6 +0,0 @@
-export const LOGIN_USER_PENDING = 'App/LOGIN_USER_PENDING';
-export const LOGIN_USER_SUCCESS = 'App/LOGIN_USER_SUCCESS';
-export const LOGIN_USER_ERROR = 'App/LOGIN_USER_ERROR';
-
-export const LOGOUT_USER = 'App/LOGOUT_USER';
-export const FORM_RESET = 'redux-form/RESET';
