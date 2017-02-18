381054d27c5cb89d2f54ef5eb87c87282e676136
Meg Drouhard <meg.drouhard@gmail.com>
2017-02-17 17:54:33 -0800

    updated HMR; not sure this is the correct solution

--- src/index.tsx
+++ src/index.tsx
@@ -6,30 +6,6 @@ import Routes from './routes';
 
 
 
-// import { App } from './App';
-
-// const appRoot = (AppComponent: typeof App) => {
-//     return (
-//         <div>
-//             <AppComponent />
-//             <DevTools />
-//         </div>
-//     );
-// };
-
-// const rootElement = document.getElementById('root');
-
-// ReactDOM.render(appRoot(App), rootElement);
-
-// if (module.hot) {
-//     // module.hot.accept();
-//     module.hot.accept("./App", () => {
-//         const NextApp = require<{ App: typeof App }>("./App").App;
-//         ReactDOM.render(appRoot(NextApp), rootElement);
-//     });
-// }
-
-
 
 ReactDOM.render(
     <Router history={browserHistory}>
@@ -38,27 +14,13 @@ ReactDOM.render(
     document.getElementById('root')
 );
 
-// import { Root } from './Root';
-
-// const routeStore = configureStore({});
-// const history = syncHistoryWithStore(browserHistory, routeStore);
-
-// ReactDOM.render(
-//     <AppContainer>
-//         <Root routeStore={routeStore} history={history} />
-//     </AppContainer>,
-//     document.getElementById('root')
-// );
-
-
-// if (module.hot) {
-//     module.hot.accept('./Root', () => {
-//         const NextRoot = require<{ Root: typeof Root }>("./Root").Root;
-//         ReactDOM.render(
-//             <AppContainer>
-//                 <NextRoot routeStore={routeStore} history={history} />
-//             </AppContainer>,
-//             document.getElementById('root')
-//         );
-//     });
-// }
+if (module.hot) {
+    module.hot.accept("./routes", () => {
+        ReactDOM.render(
+            <Router history={browserHistory}>
+                {Routes}
+            </Router>,
+            document.getElementById('root')
+        );
+    });
+}
